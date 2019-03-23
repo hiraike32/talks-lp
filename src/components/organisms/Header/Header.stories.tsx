7acee36f968c9500cb617691a18dc689c889fa9d
平池拓也 <sssb.th.63332@gmail.com>
2019-03-22 18:54:49 -0700

    feat: create Header component

---
+++ src/components/organisms/Header/Header.stories.tsx
@@ -0,0 +1,7 @@
+import { storiesOf } from "@storybook/react";
+import React from "react";
+import Header from "./Header";
+
+const story = storiesOf("organisms", module);
+
+story.add("Header", () => <Header />);
---
+++ src/components/organisms/Header/Header.tsx
@@ -0,0 +1,27 @@
+import classNames from "classnames/bind";
+import * as React from "react";
+import styles from "./Header.scss";
+import Text from "../../atoms/Text/Text";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+const cx = classNames.bind(styles);
+library.add(faBars);
+
+const Header: React.FC = () => (
+  <div className={cx("container")}>
+    <div className={cx("titleName")}>
+      <Text type="h3" bold color="lime">
+        Erick
+      </Text>
+      <Text type="h3">Wendel</Text>
+    </div>
+    <div className={cx("menu")}>
+      <Text>Menu</Text>
+      <FontAwesomeIcon icon={faBars} size="1x" color="#fff" />
+    </div>
+  </div>
+);
+
+export default Header;
