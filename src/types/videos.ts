d9d4d34b96ffbf602eb4efeb6486328340846cf4
平池拓也 <sssb.th.63332@gmail.com>
2019-03-28 15:33:30 -0700

    feat: create video thumbnail component

---
+++ src/types/videos.ts
@@ -0,0 +1,10 @@
+interface VideoJson {
+  title: string;
+  abstract: string;
+  type: "video";
+  link: string;
+  additionalLinks: string[];
+  tags: string[];
+  language: string;
+  date: string;
+}
